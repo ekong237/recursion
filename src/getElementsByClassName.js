53d1e9a6572fad28fcf84e1208813280d7d641b7
Elizabeth Kong <liz.kong@yahoo.com>
2017-08-04 20:38:58 -0700

    Stringify and ElementByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,39 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var arr = [];
+  //var bodyElement = (document.body); 
+  checkNodes(document.body);
+  return arr;
+
+  function checkNodes (node){
+  	if (node.classList.contains(className)){ 
+  		arr.push(node);
+  	}
+  	if (Array.from(node.children).length > 0){
+  		Array.from(node.children).forEach(function(current){
+  			return checkNodes(current);
+  		})
+  	}
+  }
+  
+
+
 };
+
+// use document.body, element.childNodes, element.classList
+
+
+// input classname
+// output array of el w/ class div.user p.user 
+
+// The Element.classList is a read-only property which returns a live DOMTokenList collection of the class attributes of the element.
+// Using classList is a convenient alternative to accessing an element's list of classes as a space-delimited string via element.className.
+// if classlist has classname, add to array
+// if it has childnodes, check each node classlist, repeat
+
+// childNodes property returns a colletion of a node's child nodes, as a NodeList object
+// Get a collection of the <body> element's child nodes: var c = document.body.childNodes;
+// To return a collection of a node's element nodes (excluding text and comment nodes), use the children property.
+// The children property returns a collection of an element's child elements, as an HTMLCollection object. looks like arr, but not arr
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,34 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(input) {
+  if (typeof(input) === 'boolean' || typeof(input) === 'number' || input === null){
+  		return ''+input;
+  } else if (typeof(input)==='string'){
+  		return '"' + input + '"';
+  } else if (Array.isArray(input)){    
+        var arrVals = input.reduce(function(str, el, i) {
+            if(i === input.length-1){
+            	return str + stringifyJSON(el);
+            }
+            return str + stringifyJSON(el) + ',';
+        },'');            
+        return '[' + arrVals + ']'; 
+  } else if (typeof(input) === 'function'){
+  		return '';
+  } else if (typeof(input) === 'object'){
+  		var objKeys = Object.keys(input);
+  		var objVals = objKeys.reduce(function(str, keys, i) {
+  			if (input[keys] === undefined){ return ''; }
+            if(i === objKeys.length-1){
+            	return str + stringifyJSON(keys) +':'+ stringifyJSON(input[keys]);
+            }
+            return str + stringifyJSON(keys) +':'+ stringifyJSON(input[keys])+',';
+        },''); 
+  		return '{' + objVals + '}';
+  }
 };
+
+
+
+
